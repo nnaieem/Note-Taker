8ebbfdfb40d000c02939ddb498361a238de11b19
nnaieem <nicholas.naieem@gmail.com>
2020-02-12 21:16:38 -0500

    routes

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'public', 'index.html');
+
+    res.sendFile(filePath);
+});
+
+app.get('/notes', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'public', 'notes.html');
+
+    res.sendFile(filePath);
+});
+
+app.use('*', (_, res) => {
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+    
+});
